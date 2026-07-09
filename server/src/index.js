0882a90ea70e732317cc2cc1a6dd84c073a3b92f
PurviMurkute <purvimurkute51@gmail.com>
2026-07-09 16:46:53 +0530

    Add Clerk webhook handling and integrate with user model

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,18 @@ import dotenv from "dotenv";
 import connectToDB from "./lib/db.js";
 dotenv.config();
 import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
 import job from "./lib/cron.js";
+import clerkWebhook from './webhooks/clerk.webhook.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+  "/api/webhooks/clerk",
+  express.raw({ type: "application/json" }),
+  clerkWebhook,
+);
 
 app.use(clerkMiddleware());
 app.use(cors({ origin: process.env.FRONTEND_ORIGIN, Credential: true }));
